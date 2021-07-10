f0f8d04fc807b58f0ab1693792b5b3e8977a30b9
Max Shevchencko <tier4ik@gmail.com>
2021-07-10 13:08:37 +0300

    Add NewMeetup component

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,38 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+// db link
+import DB_LINK from "../db/db-address";
+
 function NewMeetup() {
+  const history = useHistory();
+
+  async function addMeetupHandler(meetupData) {
+    try {
+      const response = await fetch(DB_LINK,
+        {
+          method: "POST",
+          body: JSON.stringify(meetupData),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      if ( !response.ok ) {
+        throw Error(response.text)
+      }
+      history.replace('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>New meetup</div>
+    <section>
+      <h1>Add new Meetup</h1>
+      <NewMeetupForm onAddMeetup={ addMeetupHandler } />
+    </section>
   )
 }
 
